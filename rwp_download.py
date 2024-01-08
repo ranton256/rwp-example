a32f57b09b3b3475c3b9af7731f762b9987aa1d9
Richard Anton <ranton@ranton.org>
2024-01-07 17:37:10 -0800

    Added downloading code

--- rwp_download.py
+++ rwp_download.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python
+import urllib3
+
 
 def main():
     print("Hello,world!")
+    my_url = "https://en.wikipedia.org/wiki/Monty_Python"
+    resp = urllib3.request("GET", my_url)
+    print(f"Response status was {resp.status}")
+    if resp.status == 200:
+        print(resp.data)
+
 
 if __name__ == "__main__":
     main()
